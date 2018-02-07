5a85b57077122408cc8b56fbf52a280c6e25ecfc
Martin Durant <martin.durant@utoronto.ca>
2018-02-07 15:37:07 -0500

    Add generic sequence reader

--- intake_avro/__init__.py
+++ intake_avro/__init__.py
@@ -3,6 +3,7 @@ import io
 from intake.source import base
 from dask import delayed
 import dask.dataframe as dd
+import dask.bag as db
 from dask.bytes import open_files
 from uavro import core as avrocore
 
@@ -21,6 +22,18 @@ class TablePlugin(base.Plugin):
                                metadata=base_kwargs['metadata'])
 
 
+class SequencePlugin(base.Plugin):
+    def __init__(self):
+        super(TablePlugin, self).__init__(
+            name='avro_sequence', version=__version__, container='python',
+            partition_access=True)
+
+    def open(self, urlpath, **kwargs):
+        base_kwargs, source_kwargs = self.separate_base_kwargs(kwargs)
+        return AvroSequenceSource(urlpath=urlpath,
+                                  metadata=base_kwargs['metadata'])
+
+
 class AvroTableSource(base.DataSource):
     """
     Source to load tabular avro datasets.
@@ -28,7 +41,7 @@ class AvroTableSource(base.DataSource):
 
     def __init__(self, urlpath, metadata=None):
         self._urlpath = urlpath
-        self._files = open_files(urlpath)
+        self._files = open_files(urlpath, mode='rb')
         self._head = None
         super(AvroTableSource, self).__init__(container='dataframe',
                                               metadata=metadata)
@@ -57,3 +70,41 @@ class AvroTableSource(base.DataSource):
         dpart = delayed(self._get_partition)
         return dd.from_delayed([dpart(i) for i in range(self.npartitions)],
                                meta=self.dtype)
+
+
+class AvroSequenceSource(base.DataSource):
+    """
+    Source to load tabular avro datasets.
+    """
+
+    def __init__(self, urlpath, metadata=None):
+        self._urlpath = urlpath
+        self._files = open_files(urlpath, mode='rb')
+        self._head = None
+        super(AvroSequenceSource, self).__init__(container='python',
+                                                 metadata=metadata)
+
+    def _get_schema(self):
+        if self._head is None:
+            with self._files[0] as f:
+                self._head = avrocore.read_header(f)
+
+        dtypes = self._head['dtypes']
+        return base.Schema(datashape=None,
+                           dtype=None,
+                           shape=None,
+                           npartitions=len(self._files),
+                           extra_metadata={})
+
+    def _get_partition(self, i):
+        with self._files[i] as f:
+            data = f.read()
+
+        import fastavro.reader
+
+        return list(fastavro.reader(io.BytesIO(data)))
+
+    def to_dask(self):
+        self.discover()
+        dpart = delayed(self._get_partition)
+        return db.from_delayed([dpart(i) for i in range(self.npartitions)])
